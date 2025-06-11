37186e7a4d3486b800ac2cabec5694817be97d93
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-11 16:10:56 -0300

    02.08 - Conhecendo os dados

---
+++ scripts/02-08-conhecendo-os-dados.sql
@@ -0,0 +1,13 @@
+USE insight_places;
+
+SELECT * FROM alugueis;
+
+SELECT * FROM avaliacoes;
+
+SELECT * FROM clientes;
+
+SELECT * FROM enderecos;
+
+SELECT * FROM hospedagens;
+
+SELECT * FROM proprietarios;
