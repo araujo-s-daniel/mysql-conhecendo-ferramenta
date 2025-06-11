df15e5a181acb44cd28e80b181a9ebc8113c319f
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-11 15:43:34 -0300

    02.04 - Criando uma tabela

---
+++ scripts/02-04-criando-uma-tabela.sql
@@ -0,0 +1,8 @@
+USE Insight_Places;
+
+CREATE TABLE proprietarios (
+    proprietario_id VARCHAR (255) PRIMARY KEY,
+    nome VARCHAR (255),
+    cpf_cnpj VARCHAR (20),
+    contato VARCHAR (255)
+);
